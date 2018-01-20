18398496ba77968ed76192d0a44b46a291e9f6f1
Asif Bilakhiya <24765668+bilakhiyaasif@users.noreply.github.com>
2018-01-20 07:32:49 +0530

    Create TestClass.java

---
+++ ASCII Value/TestClass.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class TestClass
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.printf("%d",(int)(sc.next().charAt(0)));
+
+  }
+}
